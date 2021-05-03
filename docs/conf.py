0399162ff2f5e2ae9653578267a4a7f2dcab9fb1
Eugene Lyapustin <unishifft@gmail.com>
2021-05-03 14:22:06 +0300

    Make Sphinx config look good

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,15 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'CoVid'
-copyright = '2021, Eugene Lyapustin, Egor Sklyarov, Alexey Solovyev'
-author = 'Eugene Lyapustin, Egor Sklyarov, Alexey Solovyev'
+project = "CoVid"
+copyright = "2021, Eugene Lyapustin, Egor Sklyarov, Alexey Solovyev"
+author = "Eugene Lyapustin, Egor Sklyarov, Alexey Solovyev"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,14 +29,14 @@ author = 'Eugene Lyapustin, Egor Sklyarov, Alexey Solovyev'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.githubpages'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -48,9 +49,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
