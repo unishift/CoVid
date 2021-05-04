6c3d2d05f839e698a999b84f433f409619b3f011
Alexey Solovyov <alexey.soloviev@graphics.cs.msu.ru>
2021-05-04 04:12:10 +0300

    Can now select and play videos

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
 import gettext
+import time
 import tkinter as tk
+from tkinter import filedialog, messagebox
 
+from PIL import ImageTk, Image
+import numpy as np
+
+import compose
+import video_reader
 
 gettext.install("covid", os.path.dirname(__file__))
 
@@ -34,6 +41,20 @@ class Application(tk.Frame):
 
 
 class App(Application):
+    def __init__(self, *args, **kwargs):
+        super(App, self).__init__(*args, **kwargs)
+
+        self.left_video: video_reader.FfmsReader = None
+        self.right_video: video_reader.FfmsReader = None
+        self.paused = False
+        self.last_image = None
+        self.need_reset_last_image = False
+        self.composer = compose.Composer(
+            compose.ComposeVerticalSplit,
+            info_provide_func=lambda *args: "PSNR:24.07070707\nSSIM:228.14888",
+            font_config=compose.FontConfig(),
+        )
+
     def create_widgets(self):
         super().create_widgets()
         self.create_menu()
@@ -63,18 +84,147 @@ class App(Application):
         )
         self.offset_box.grid(row=1, column=2)
 
+        self.master.bind("<Configure>", self.handle_resize)
+
     def configure_widgets(self):
         super().configure_widgets()
         self.rowconfigure(1, weight=0)  # fixed controls height
         self.controls.columnconfigure(2, weight=1)
+
+    def _select_video_safe(self):
+        file_name = filedialog.askopenfilename()
+        if file_name is None:
+            return
+        try:
+            return video_reader.FfmsReader(file_name)
+        except video_reader.VideoOpenException as e:
+            messagebox.showerror("Error!", f"Can't open {file_name} as video")
+        return None
+
+    def _sync_progress_bar_length(self):
+        left_length = self.left_video.GetLength()
+        right_length = self.right_video.GetLength()
+
+        offset = int(self.offset.get())
+
+        self.timeline.config(
+            from_=max(-offset, 0), to=min(left_length - 1, right_length - 1 - offset)
+        )
+
+    def _sync_video_with_offset(self):
+        assert self.left_video is not None and self.right_video is not None
+        current_delta = (
+            self.right_video.GetPlaybackFramePosition()
+            - self.left_video.GetPlaybackFramePosition()
+        )
+        desired_delta = int(self.offset.get())
+        if (
+            current_delta != desired_delta
+        ):  # If we need to fix offset, at first we try to move right video
+            self.right_video.ShiftPlaybackFramePosition(desired_delta - current_delta)
+        current_delta = (
+            self.right_video.GetPlaybackFramePosition()
+            - self.left_video.GetPlaybackFramePosition()
+        )
+        if current_delta != desired_delta:  # If it's not enough, we move left video
+            self.left_video.ShiftPlaybackFramePosition(-(desired_delta - current_delta))
+        current_delta = (
+            self.right_video.GetPlaybackFramePosition()
+            - self.left_video.GetPlaybackFramePosition()
+        )
+        if (
+            current_delta != desired_delta
+        ):  # Finally we understand that we can't fix offset and change the value back
+            self.offset.set(str(current_delta))
+        # Now we need to fix maximum progress_bar value as the offset might have an impact on it
+        self._sync_progress_bar_length()
+
+    def _videos_next_frame(self):
+        canvas_size_wh = self.C.winfo_width(), self.C.winfo_height()
+        if self.left_video is None and self.right_video is None:
+            return
+        if self.left_video is not None:
+            left_frame, left_delta = self.left_video.GetNextFrame()
+        if self.right_video is not None:
+            right_frame, right_delta = self.right_video.GetNextFrame()
+
+        if self.left_video is None:
+            left_frame = np.zeros_like(right_frame)
+            left_delta = 1000 / 24
+        elif self.right_video is None:
+            right_frame = np.zeros_like(left_frame)
+            right_delta = 1000 / 24
+        elif self.left_video.IsEnd() or self.right_video.IsEnd():
+            return
+
+        composed = self.composer.Compose(
+            left_frame, right_frame, canvas_size_wh=canvas_size_wh
+        )
+        # resize_coeff = min(canvas_size_wh[0] / composed.width, canvas_size_wh[1] / composed.height)
+        # composed = composed.resize(size=(int(composed.width * resize_coeff), int(composed.height * resize_coeff)),
+        #                           resample=Image.BILINEAR)
+        if self.last_image is None or self.need_reset_last_image:
+            self.need_reset_last_image = False
+            self.last_image = ImageTk.PhotoImage(composed)
+            self.C.create_image(0, 0, anchor="nw", image=self.last_image)
+        else:
+            self.last_image.paste(composed)
+        self.C.update()
+        return left_delta
+
+    def handle_resize(self, event):
+        canvas_size_wh = self.C.winfo_width(), self.C.winfo_height()
+        if self.left_video is not None:
+            self.left_video.UpdateVideoSize(canvas_size_wh)
+            self.left_video.ShiftPlaybackFramePosition(-1)
+        if self.right_video is not None:
+            self.right_video.UpdateVideoSize(canvas_size_wh)
+            self.riht_video.ShiftPlaybackFramePosition(-1)
+        self._videos_next_frame()
+        self.need_reset_last_image = True
+
+    def video_playback_update(self):
+        if self.paused:
+            return
+        if self.left_video is not None and self.right_video is not None:
+            cur_time = time.time()
+            left_delta = self._videos_next_frame()
+            elapsed_time = (time.time() - cur_time) * 1000
+            print(elapsed_time)
+            self.master.after(
+                int(max(left_delta - elapsed_time, 1)), self.video_playback_update
+            )
+
+    def _check_start_timer(self, delay):
+        if self.left_video is not None and self.right_video is not None:
+            self.master.after(int(delay), self.video_playback_update)
+
+    def _on_select_canvas_update(self, first_video, second_video):
+        if second_video is not None:
+            second_video.SetPlaybackFramePosition(0)
+            self._sync_video_with_offset()
+            self._sync_progress_bar_length()
+        if first_video is not None:
+            canvas_size_wh = self.C.winfo_width(), self.C.winfo_height()
+            first_video.UpdateVideoSize(canvas_size_wh)
+            left_delta = self._videos_next_frame()
+            self._check_start_timer(left_delta)
+
+    def select_left_video(self):
+        self.left_video = self._select_video_safe()
+        self._on_select_canvas_update(self.left_video, self.right_video)
+
+    def select_right_video(self):
+        self.right_video = self._select_video_safe()
+        self._on_select_canvas_update(self.right_video, self.left_video)
 
     def create_menu(self):
         menu_bar = tk.Menu(self)
         self.master.config(menu=menu_bar)
 
         file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label=_("Open left"), command=None)
-        file_menu.add_command(label=_("Open right"), command=None)
+        file_menu.add_command(label=_("Open left"), command=self.select_left_video)
+        file_menu.add_command(label=_("Open right"), command=self.select_right_video)
         file_menu.add_separator()
         file_menu.add_command(label=_("Save as GIF..."), command=None)
         file_menu.add_command(label=_("Save as video..."), command=None)
